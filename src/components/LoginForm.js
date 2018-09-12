bd942708c0b9dbfbb12d4615b87366a37274b0d8
aditiadika <aditiaaadicka@gmail.com>
2018-09-12 15:45:31 +0700

    implement redux in email text

---
+++ src/components/LoginForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { Card, CardSection, Input, Button } from './common'
+import { connect } from 'react-redux'
+import { emailChanged } from '../actions'
+
+class LoginForm extends Component {
+    onEmailChange(text) {
+        this.props.emailChanged(text)
+    }
+
+    render() {
+        return (
+        <Card>
+            <CardSection>
+                <Input 
+                    placeHolder="john@example.com"
+                    label="Email"
+                    onChangeText={this.onEmailChange.bind(this)}
+                    value={this.props.email}
+                />
+            </CardSection>
+
+            <CardSection>
+                <Input
+                    secureTextEntry 
+                    placeHolder="Password"
+                    label="password"
+                />
+            </CardSection>
+
+            <CardSection>
+                <Button>Login</Button>
+            </CardSection>
+            
+        </Card>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        email: state.auth.email
+    }
+}
+
+export default connect(mapStateToProps, { emailChanged })(LoginForm)
